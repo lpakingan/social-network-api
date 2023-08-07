eedcf33f5e5adc8904461b17eb06daa1115238ed
lpakingan <lpak98@gmail.com>
2023-08-07 10:21:04 -0700

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 const { connect, connection } = require("mongoose");
 
-// connect to the specified mongodb database
-connect("mongodb://127.0.0.1:27017/userThoughts");
+// creates a connection string for connecting to the social network database
+// will connect locally if there is no heroku link
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialNetworkDB";
+
+connect(connectionString);
 
 module.exports = connection;
